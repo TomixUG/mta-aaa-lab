e6773c87cf07615b562df506e7e1f8c0fc302932
TomixUG <tomas.sousek1@gmail.com>
2024-07-08 20:49:11 +0200

    add logout

---
+++ src/routes/logout/+page.server.ts
@@ -0,0 +1,8 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+import constants from "$lib/constants";
+
+export const load: PageServerLoad = async ({ cookies }) => {
+  cookies.delete(constants.cookieName, { path: "/", secure: false });
+  redirect(302, "/");
+};
